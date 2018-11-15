7edd012001d9aba39f3f1f62a22ac394f1a2f121
Sarthak <sarthak@DESKTOP-R5FJ5RJ.localdomain>
2018-11-15 14:10:53 -0500

    prog3 ex0 done

--- CSE3300/Exercise0/client.py
+++ CSE3300/Exercise0/client.py
@@ -1,24 +1,46 @@
 from socket import *
 import random
 
+#gets the computers IP address
 ipLocal = gethostbyname(gethostname())
 
 serverName = 'tao.ite.uconn.edu'
 serverPort = 3300
 
+#new socket and connecting to the given server name and server port
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
+#generating a random number
 usernum = random.randint(1000, 8000)
+
+#constructing request string to send to server
 requestString = 'ex0 137.99.3.212-%s %s %s S.Bhatnagar\n' %(serverPort, ipLocal, usernum)
 print requestString
 clientSocket.send(requestString)
 
+#getting the reply from the server
 modifiedString = clientSocket.recv(1024)
-if not(modifiedString.find('OK')):
-       print 'No OK'
-print 'From Server: %s' %(modifiedString)   
+print 'From Server: ', modifiedString
 
+#getting the servernum
+servernumIndex = modifiedString.find('gar')
+servernum = int(modifiedString[servernumIndex + 4 : ])
+
+#checking if OK was in the recv. creating ackString with incremented usernum and servernum
+if (modifiedString.find('OK') != -1):
+   # newusernum = usernum+2
+   # newservernum = servernum+1
+    print 'found OK'
+    ackString = 'ex0 %s %s\n' %((usernum+2), (servernum+1))
+    print ackString
+    clientSocket.send(ackString)
+    serverAckString = clientSocket.recv(1024)
+    print serverAckString
+else:
+    print 'Error No OK'
+    print 'From server: %s' %(moddifiedString)
+    
 clientSocket.close()
 
 
