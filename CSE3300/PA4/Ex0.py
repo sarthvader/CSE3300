7d26dbdf81128853616fe39e80792f41e9293bf1
Sarthak <sarthak@uconn.edu>
2018-12-09 16:42:47 +0000

    struct progress

--- CSE3300/PA4/Ex0.py
+++ CSE3300/PA4/Ex0.py
@@ -2,17 +2,19 @@ from socket import *
 import random
 import struct
 
-serverName = 'tao.uits.uconn.edu'
+serverName = 'tao.ite.uconn.edu'
 serverPort = 3300
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+timeout = 5.0
+transmissionCount = 5
 
 # message struc
-msgServer = 00110011100100
+msgServer = 3300
 msgType = 0
 msgRequestFlag = 0
-msgLabNum = 0000
-msgVersion = 0111
-
+msgLab = 4
+msgVersion = 7
+msgLabAndVersion = 0407 
 requestData = input('Enter your SSN:')
 print ('Your SSN is '), requestData
 
@@ -20,6 +22,17 @@ checkSum = 0
 clientCookie = random.randint(10000000,80000000)
 result = 0
 
-message = struct.pack('!2H2I2H', msgType, msgRequestFlag, msgServer, msgLabNum, clientCookie, requestData, checkSum, result)
+message = struct.pack('!3H2I2H', msgServer, msgLab, msgVersion, clientCookie, requestData, checkSum, result)
 
-print(message)
+print struct.unpack('!3H2I2H', message)
+
+while(transmissionCount > 0):
+    clientSocket.sendto(message, (serverName, serverPort))
+    clientSocket.settimeout(timeout)
+    responseMessage, serverAddress = clientSocket.recvfrom(1024)
+    print ('Server Response: '), struct.unpack('!3H2I3H', responseMessage)
+    if socket.timeout:
+        print ('Timeout count = 1. Sending again')
+        transmissionCount-=1
+
+
