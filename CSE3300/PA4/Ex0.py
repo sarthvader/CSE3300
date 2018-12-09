c54dc11c6512b01fd37c26c2cc0f54653729c1d4
Sarthak <sarthak@uconn.edu>
2018-12-09 23:36:57 +0000

    Part 0 Fin

--- CSE3300/PA4/Ex0.py
+++ CSE3300/PA4/Ex0.py
@@ -3,18 +3,20 @@ import random
 import struct
 
 serverName = 'tao.ite.uconn.edu'
-serverPort = 3300
+serverIP = gethostbyname(serverName)
+serverPort = 3301
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 timeout = 5.0
 transmissionCount = 5
+timeoutCount = 0
 
 # message struc
 msgServer = 3300
 msgType = 0
 msgRequestFlag = 0
-msgLab = 4
-msgVersion = 7
-msgLabAndVersion = 0407 
+msgLab = 4 #0100
+msgVersion = 7 #0111
+msgLabAndVersion = 1031
 requestData = input('Enter your SSN:')
 print ('Your SSN is '), requestData
 
@@ -22,17 +24,21 @@ checkSum = 0
 clientCookie = random.randint(10000000,80000000)
 result = 0
 
-message = struct.pack('!3H2I2H', msgServer, msgLab, msgVersion, clientCookie, requestData, checkSum, result)
+message = struct.pack('!2H2I2H', msgServer, msgLabAndVersion, clientCookie, requestData, checkSum, result)
 
-print struct.unpack('!3H2I2H', message)
+print struct.unpack('!2H2I2H', message)
 
 while(transmissionCount > 0):
-    clientSocket.sendto(message, (serverName, serverPort))
-    clientSocket.settimeout(timeout)
-    responseMessage, serverAddress = clientSocket.recvfrom(1024)
-    print ('Server Response: '), struct.unpack('!3H2I3H', responseMessage)
-    if socket.timeout:
-        print ('Timeout count = 1. Sending again')
-        transmissionCount-=1
-
+    clientSocket.sendto(message, (serverIP, serverPort))
+    try:
+        clientSocket.settimeout(timeout)
+        responseMessage, serverAddress = clientSocket.recvfrom(1024)
+        print ('Server Response: '), struct.unpack('!2H2I2H', responseMessage)
+        
+    except timeout:
+        print ('Timeout. Sending again')
+        timeoutCount += 1
+    transmissionCount -= 1
+    if (transmissionCount < 5):
+        break
 
